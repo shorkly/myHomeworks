4828b9f3dfe2197ba71c2f4ebddc7ddc956fdd1b
Ann <iykh499@gmail.com>
2020-11-02 14:48:42 +0200

    added 12-14 homework

---
+++ 14/Cahee/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $(".hamburger").click(function(){
+        $(".hamburger").toggleClass("is-active");
+        $("#mobile_menu, #menu_shadow").toggleClass("opened");
+    });
+
+    $("#menu_shadow").click(function () {
+        $(".hamburger").removeClass("is-active");
+        $("#mobile_menu, #menu_shadow").removeClass("opened");
+    });
+
+    $(window).scroll(function(){
+        if($(window).scrollTop()>50){
+            $("header").addClass("scrolled");
+        }else{
+            $("header").removeClass("scrolled");
+        }
+    });
+});
